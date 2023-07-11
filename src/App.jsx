4d6962a56ee5ba3d3c74ad0c560299974664db26
Lautaro Haedo <haedo2346@hotmail.es>
2023-07-11 09:34:55 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./routes/Login";
+import Home from "./routes/Home";
+import Navbar from "./components/Navbar";
+import RequireAuth from "./components/requireAuth";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>App</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
